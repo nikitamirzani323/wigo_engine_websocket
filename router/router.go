50014c28f91b2f62204c85e605d5c31d03e128ba
nikitamirzani323 <nikitamirzani323@gmail.com>
2024-03-22 01:41:53 +0700

    new websocket to agen

--- router/router.go
+++ router/router.go
@@ -1,7 +1,6 @@
 package router
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"strconv"
@@ -13,7 +12,6 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/valyala/fasthttp"
 )
 
 type Server struct {
@@ -29,59 +27,13 @@ func Init() *fiber.App {
 
 	app.Use("ws/", AllowUpgrade)
 	app.Use("ws/time", websocket.New(handleWebsocket(&Server{})))
-	app.Get("/sse", func(c *fiber.Ctx) error {
-		c.Set("Content-Type", "text/event-stream")
-		c.Set("Cache-Control", "no-cache")
-		c.Set("Connection", "keep-alive")
-		c.Set("Transfer-Encoding", "chunked")
+	app.Use("ws/invoiceedit", websocket.New(handleWebsocketInvoiceEdit(&Server{})))
 
-		c.Context().SetBodyStreamWriter(fasthttp.StreamWriter(func(w *bufio.Writer) {
-			fmt.Println("WRITER")
-
-			dbHost := os.Getenv("DB_REDIS_HOST") + ":" + os.Getenv("DB_REDIS_PORT")
-			dbPass := os.Getenv("DB_REDIS_PASSWORD")
-			dbName, _ := strconv.Atoi(os.Getenv("DB_REDIS_NAME"))
-
-			rdb := redis.NewClient(&redis.Options{
-				Addr:     dbHost,
-				Password: dbPass,
-				DB:       dbName,
-			})
-
-			resultredis := rdb.Subscribe("", "payload_nuke")
-			defer resultredis.Close()
-			for {
-				msg, err := resultredis.ReceiveMessage()
-				if err != nil {
-					panic(err)
-				}
-
-				// fmt.Println("Received message from " + msg.Payload + " channel.")
-				// data_pubsub := strings.Split(msg.Payload, ":")
-
-				msg_sse := msg.Payload
-
-				fmt.Fprintf(w, "data: %s\n\n", msg_sse)
-				// fmt.Println(msg_sse)
-				err_sse := w.Flush()
-				if err_sse != nil {
-					fmt.Printf("Error while flushing: %v. Closing http connection.\n", err)
-
-					break
-				}
-				// time.Sleep(1 * time.Second)
-
-			}
-
-		}))
-
-		return nil
-	})
 	return app
 }
 func handleWebsocket(s *Server) func(ws *websocket.Conn) {
 	return func(ws *websocket.Conn) {
-		fmt.Println("New incoming connection from client to orderbook feed:", ws.RemoteAddr())
+		fmt.Println("New incoming connection from client to timer feed:", ws.RemoteAddr())
 
 		msgType, mr, err := ws.ReadMessage()
 		if err != nil {
@@ -91,6 +43,52 @@ func handleWebsocket(s *Server) func(ws *websocket.Conn) {
 			// log.Println(string(mr))
 		}
 		keyredis := "payload_" + string(mr)
+		dbHost := os.Getenv("DB_REDIS_HOST") + ":" + os.Getenv("DB_REDIS_PORT")
+		dbPass := os.Getenv("DB_REDIS_PASSWORD")
+		dbName, _ := strconv.Atoi(os.Getenv("DB_REDIS_NAME"))
+
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     dbHost,
+			Password: dbPass,
+			DB:       dbName,
+		})
+
+		resultredis := rdb.Subscribe("", keyredis)
+		defer resultredis.Close()
+
+		for {
+			msg, err := resultredis.ReceiveMessage()
+			if err != nil {
+				panic(err)
+			}
+			msg_sse := msg.Payload
+			msg_replace := strings.Replace(msg_sse, `"`, "", 2)
+			// fmt.Println(msg_replace)
+
+			payload := fmt.Sprintf(msg_replace)
+			ws.WriteJSON(payload)
+			// time.Sleep(time.Second * 1)
+			// err = ws.WriteMessage(msg_sse, []byte{msg_sse})
+			// if err != nil {
+			// 	log.Println("write:", err)
+			// 	break
+			// }
+
+		}
+	}
+}
+func handleWebsocketInvoiceEdit(s *Server) func(ws *websocket.Conn) {
+	return func(ws *websocket.Conn) {
+		fmt.Println("New incoming connection from agen to invoice edit:", ws.RemoteAddr())
+
+		msgType, mr, err := ws.ReadMessage()
+		if err != nil {
+			return
+		}
+		if msgType == websocket.TextMessage {
+			// log.Println(string(mr))
+		}
+		keyredis := "payload_agen_" + string(mr)
 		dbHost := os.Getenv("DB_REDIS_HOST") + ":" + os.Getenv("DB_REDIS_PORT")
 		dbPass := os.Getenv("DB_REDIS_PASSWORD")
 		dbName, _ := strconv.Atoi(os.Getenv("DB_REDIS_NAME"))
